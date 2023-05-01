5019b07bb455f74b28a2b65c72a45f6ed6897421
prod3v3loper <16712059+prod3v3loper@users.noreply.github.com>
2023-05-01 16:16:37 +0200

    Update
    - Change templates path
    - NodeJS CI
    - Add npm packages

--- app/settings/web3/package-json.js
+++ app/settings/web3/package-json.js
@@ -12,25 +12,22 @@ module.exports = function createPackageJsonWeb3(answer) {
       ? "'" + answer.description + "'"
       : "This is a Webpack Scaffolding in pro style with separated configs and better understandable in complex environments.",
     version: answer.version ? "'" + answer.version + "'" : "1.0.0",
-    author: answer.author
-      ? "'" + answer.author + "'"
-      : "Samet Tarim aka prod3v3loper",
+    author: answer.author ? "'" + answer.author + "'" : "Samet Tarim aka prod3v3loper",
     private: answer.private ? "'" + answer.private + "'" : true,
     keywords: answer.keywords ? answer.keywords : ["web3-contract-dapp"],
     license: answer.license ? "'" + answer.license + "'" : "MIT License",
     scripts: {
-      remove: "rm -rf build/",
-      build: "truffle compile",
-      dev: "truffle migrate --reset --network develop",
-      testnet: "truffle migrate --reset --network testnet",
-      mainnet: "truffle migrate --reset --network mainnet",
-      test: "echo \"Error: no test specified\" && exit 1"
+        remove: "rm -rf build/",
+        build: "truffle compile",
+        dev: "truffle migrate --reset --network develop",
+        testnet: "truffle migrate --reset --network testnet",
+        mainnet: "truffle migrate --reset --network mainnet",
+        test: "echo \"Error: no test specified\" && exit 1"
     },
-    dependencies: {
-      "@truffle/hdwallet-provider": "^1.2.3",
-      "dotenv": "^16.0.3"
-    },
+    dependencies: {},
     devDependencies: {
+        "dotenv": "^16.0.3",
+        "@truffle/hdwallet-provider": "^1.2.3",
         "truffle-plugin-verify": "^0.6.2"
     }
   };
